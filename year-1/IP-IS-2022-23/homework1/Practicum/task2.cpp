7918362fba25c6c55da88cfe13dbc2fa5a3bbaaa
vasil.paligorov <vasil.paligorov@excitel.tech>
2022-12-27 11:37:26 +0200

    Fix task2 from homework1 Practicum

--- year-1/IP-IS-2022-23/homework1/Practicum/task2.cpp
+++ year-1/IP-IS-2022-23/homework1/Practicum/task2.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
-#include <ctime>
 #include <cmath>
 
 using namespace std;
 
 int rollDice() {
-    srand(time(nullptr));
     int number = 0;
+
     for (int i = 0; i < 5; ++i)
         number += (rand() % 6 + 1) * (int) pow(10, i);
+
     return number;
 }
 
 bool checkThreeIdentical(int number) {
     int digits[6] = {0};
+
     for (; number; number /= 10)
         digits[number % 10 - 1]++;
 
     for (int i = 0; i < 6; ++i)
         if (digits[i] > 2)
             return true;
+
     return false;
 }
 
 int calculateDiceRolls() {
     int rolls = 0;
+
     for (int i = 0; i != 3; rolls++)
         checkThreeIdentical(rollDice()) ? i++ : i = 0;
+
     return rolls;
 }
 
 int main() {
+    srand(time(nullptr));
     cout << "Pencho, you will need " << calculateDiceRolls() << " dice rolls.";
 }
